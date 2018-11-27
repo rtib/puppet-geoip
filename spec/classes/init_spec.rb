6a0ddb9391e2d036d46bcf5daa59cc1606966ddd
Tibor Répási <rtib@users.noreply.github.com>
2018-11-27 12:04:14 +0100

    rubocop autocorrect

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,12 +1,11 @@
 require 'spec_helper'
 describe 'geoip' do
-  on_supported_os.each do |os,default_facts|
+  on_supported_os.each do |os, default_facts|
     context "on #{os}" do
       let(:facts) do
-        default_facts.merge({
-          :service_provider => 'systemd'
-        })
+        default_facts.merge(service_provider: 'systemd')
       end
+
       context 'with default values' do
         it { is_expected.to compile.with_all_deps }
         it { is_expected.to contain_class('geoip') }
