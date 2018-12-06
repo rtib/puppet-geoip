5c5b2f24b40c00555b45e3c6e0c83a4d230611eb
Tibor Répási <rtib@users.noreply.github.com>
2018-12-06 12:23:42 +0100

    file existence checks

--- spec/acceptance/02_update_spec.rb
+++ spec/acceptance/02_update_spec.rb
@@ -4,13 +4,12 @@ describe 'geoip update' do
   describe 'systemd service' do
     subject(:systemd_unit) { command('systemctl start geoip_update.service') }
 
-    its(:exit_status) { should eq 0 }
+    its(:exit_status) { is_expected.to eq 0 }
   end
 
   describe 'geoipupdate command' do
     subject(:systemd_unit) { command('geoipupdate -v') }
 
-    its(:exit_status) { should eq 0 }
+    its(:exit_status) { is_expected.to eq 0 }
   end
-  
 end
---
+++ spec/acceptance/03_files_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_acceptance_helper'
+
+describe 'geoip database files' do
+  describe file('/var/lib/GeoIP/GeoLite2-Country.mmdb') do
+    it { is_expected.to exist }
+    it { is_expected.to be_file }
+    it { is_expected.to be_readable }
+    its(:size) { should > 0 }
+  end
+  describe file('/var/lib/GeoIP/GeoLite2-City.mmdb') do
+    it { is_expected.to exist }
+    it { is_expected.to be_file }
+    it { is_expected.to be_readable }
+    its(:size) { should > 0 }
+  end
+end
