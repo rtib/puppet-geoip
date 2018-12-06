ff45af10593d6751b333b85ba1ec8c6ccd3c077c
Tibor Répási <rtib@users.noreply.github.com>
2018-12-06 15:45:16 +0100

    whitespaces

--- spec/acceptance/04_lookup_spec.rb
+++ spec/acceptance/04_lookup_spec.rb
@@ -7,17 +7,17 @@ describe 'geoip update' do
     context "against database #{db}" do
       describe 'valid IP address' do
         subject(:valid_country_lookup) { command("mmdblookup --file /var/lib/GeoIP/#{db}.mmdb --ip 8.8.8.8") }
-    
+
         its(:exit_status) { is_expected.to eq 0 }
       end
       describe 'unknown IP address' do
         subject(:valid_country_lookup) { command("mmdblookup --file /var/lib/GeoIP/#{db}.mmdb --ip 127.0.0.1") }
-    
+
         its(:exit_status) { is_expected.to eq 6 }
       end
       describe 'invalid IP address' do
         subject(:valid_country_lookup) { command("mmdblookup --file /var/lib/GeoIP/#{db}.mmdb --ip 256.0.0.1") }
-    
+
         its(:exit_status) { is_expected.to eq 3 }
       end
     end
