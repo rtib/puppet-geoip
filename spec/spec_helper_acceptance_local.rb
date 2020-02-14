0ee867c31eae16a6a517a3d65cc6caf73bebfdc1
Tibor Répási <rtib@users.noreply.github.com>
2020-02-14 09:40:13 +0100

    version switched testing

--- spec/spec_helper_acceptance_local.rb
+++ spec/spec_helper_acceptance_local.rb
@@ -2,14 +2,15 @@
 
 include PuppetLitmus
 
-DBS = ['GeoLite2-Country', 'GeoLite2-City'].freeze
+# constants
+DBS = ['GeoLite2-ASN', 'GeoLite2-Country', 'GeoLite2-City'].freeze
+CONFIG_GE311 = false # default
 
-# node_vars = vars_from_node(inventory_hash_from_inventory_file, ENV['TARGET_HOST']) || {}
-
-# if node_vars.key?('apt_sources_list')
-#  bolt_upload_file(File.join(File.dirname(__FILE__), 'fixtures', node_vars['apt_sources_list']), '/etc/apt/sources.list.d/cassandra.sources.list')
-#  run_shell('apt-get update')
-# end
-
-run_shell('cat /etc/apt/sources.list | cut -d" " -f-3 | sed "s/$/ contrib non-free/g" > /etc/apt/sources.list.d/extra.list')
-run_shell('apt-get update')
+node_facts = facts_from_node(inventory_hash_from_inventory_file, ENV['TARGET_HOST']) || {}
+if node_facts['platform'] =~ %r{debian}
+  run_shell('cat /etc/apt/sources.list | cut -d" " -f-3 | sed "s/$/ contrib non-free/g" > /etc/apt/sources.list.d/extra.list')
+  run_shell('apt-get update')
+  if node_facts['platform'] =~ %r{debian:10}
+    CONFIG_GE311 = true
+  end
+end
