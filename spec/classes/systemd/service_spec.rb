b42c2eaa608fd781214df8116956991f530a43a6
Tibor Répási <rtib@users.noreply.github.com>
2021-03-25 14:01:36 +0100

    add user and group to update service (#27)

--- spec/classes/systemd/service_spec.rb
+++ spec/classes/systemd/service_spec.rb
@@ -8,39 +8,74 @@ describe 'geoip' do
       context "on #{os}" do
         let(:facts) { os_facts }
 
-        let(:params) do
-          default_config(os)
+        context 'default config' do
+          let(:params) do
+            default_config(os)
+          end
+
+          it { is_expected.to compile }
+          context 'systemd unit file contents' do
+            it do
+              is_expected.to contain_systemd__unit_file('geoip_update.service')
+                .with('ensure' => 'present')
+            end
+            it do
+              is_expected.to contain_systemd__unit_file('geoip_update.service')
+                .with_content(%r{^Type=exec$})
+            end
+            it do
+              is_expected.to contain_systemd__unit_file('geoip_update.service')
+                .with_content(%r{^User=root$})
+            end
+            it do
+              is_expected.to contain_systemd__unit_file('geoip_update.service')
+                .with_content(%r{^Group=root$})
+            end
+            it do
+              is_expected.to contain_systemd__unit_file('geoip_update.service')
+                .with_content(%r{^ExecStart=/usr/bin/geoipupdate -v -f /etc/GeoIP.conf$})
+            end
+            it do
+              is_expected.to contain_systemd__unit_file('geoip_update.service')
+                .with_content(%r{^Restart=on-failure$})
+            end
+            it do
+              is_expected.to contain_systemd__unit_file('geoip_update.service')
+                .with_content(%r{^RestartSec=5min$})
+            end
+            it do
+              is_expected.to contain_systemd__unit_file('geoip_update.service')
+                .with_content(%r{^StandardOutput=journal$})
+            end
+            it do
+              is_expected.to contain_systemd__unit_file('geoip_update.service')
+                .with_content(%r{^StandardError=journal$})
+            end
+          end
         end
 
-        it { is_expected.to compile }
-        context 'systemd unit file contents' do
-          it do
-            is_expected.to contain_systemd__unit_file('geoip_update.service')
-              .with('ensure' => 'present')
+        context 'alternate user/group config' do
+          let(:params) do
+            default_config(os).merge(
+              'service_user'  => 'geo_user',
+              'service_group' => 'geo_group',
+            )
           end
-          it do
-            is_expected.to contain_systemd__unit_file('geoip_update.service')
-              .with_content(%r{^Type=exec$})
-          end
-          it do
-            is_expected.to contain_systemd__unit_file('geoip_update.service')
-              .with_content(%r{^ExecStart=/usr/bin/geoipupdate -v -f /etc/GeoIP.conf$})
-          end
-          it do
-            is_expected.to contain_systemd__unit_file('geoip_update.service')
-              .with_content(%r{^Restart=on-failure$})
-          end
-          it do
-            is_expected.to contain_systemd__unit_file('geoip_update.service')
-              .with_content(%r{^RestartSec=5min$})
-          end
-          it do
-            is_expected.to contain_systemd__unit_file('geoip_update.service')
-              .with_content(%r{^StandardOutput=journal$})
-          end
-          it do
-            is_expected.to contain_systemd__unit_file('geoip_update.service')
-              .with_content(%r{^StandardError=journal$})
+
+          it { is_expected.to compile }
+          context 'systemd unit file contents' do
+            it do
+              is_expected.to contain_systemd__unit_file('geoip_update.service')
+                .with('ensure' => 'present')
+            end
+            it do
+              is_expected.to contain_systemd__unit_file('geoip_update.service')
+                .with_content(%r{^User=geo_user$})
+            end
+            it do
+              is_expected.to contain_systemd__unit_file('geoip_update.service')
+                .with_content(%r{^Group=geo_group$})
+            end
           end
         end
       end
