69eb1396c87d04f072052ba614895f5d6ba0fd41
Tibor Répási <rtib@users.noreply.github.com>
2021-03-25 13:47:46 +0100

    add user and group to update service

--- manifests/init.pp
+++ manifests/init.pp
@@ -29,6 +29,8 @@
 # @param config_version the config version to use for geoipupdate
 # @param config hash of configuration options
 # @param manage_service whether to manage database updating service
+# @param service_user effective user the update service should run
+# @param serivce_group effective group the update service should run
 # @param update_path path to the geoipupdate tool, used by update service
 # @param service_name name of the update service
 # @param update_timers wallclock timers when the update service should be triggered (for syntax see [systemd.time(7)#Parsing Timestamps](https://www.freedesktop.org/software/systemd/man/systemd.time.html#Parsing%20Timestamps))
@@ -42,6 +44,8 @@ class geoip (
   Enum['lt311','ge311']     $config_version = 'lt311',
   Hash                      $config = {},
   Boolean                   $manage_service = true,
+  String                    $service_user = 'root',
+  String                    $service_group = 'root',
   Stdlib::Absolutepath      $update_path = '/usr/bin/geoipupdate',
   String                    $service_name = 'geoip_update',
   Array[String]             $update_timers = [],
